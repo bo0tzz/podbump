b1a9ae725846d98a79652ea7520afb1b984397df
bo0tzz <git@bo0tzz.me>
2023-01-21 17:49:15 +0100

    Initial commit

---
+++ lib/podbump.ex
@@ -0,0 +1,18 @@
+defmodule Podbump do
+  @moduledoc """
+  Documentation for `Podbump`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Podbump.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/podbump_test.exs
@@ -0,0 +1,8 @@
+defmodule PodbumpTest do
+  use ExUnit.Case
+  doctest Podbump
+
+  test "greets the world" do
+    assert Podbump.hello() == :world
+  end
+end
