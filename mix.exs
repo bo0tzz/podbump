fc1d52a43eaa33c33425d666ad0a4c4032614c6c
bo0tzz <git@bo0tzz.me>
2023-01-21 21:19:18 +0100

    v0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Podbump.MixProject do
   def project do
     [
       app: :podbump,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
