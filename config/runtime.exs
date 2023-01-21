516974d6a0327ab8a73459c5c46a8e15ae0f92bd
bo0tzz <git@bo0tzz.me>
2023-01-21 20:17:30 +0100

    Set up docker build

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,8 +3,10 @@ import Config
 get_k8s_conn = fn ->
   {m, f, a} = Application.fetch_env!(:podbump, Podbump.Kubernetes)[:conn_args]
 
-  {:ok, conn} = apply(m, f, a)
-  conn
+  case apply(m, f, a) do
+    {:ok, conn} -> conn
+    {:error, err} -> raise "Failed to load kubernetes configuration: #{inspect(err)}"
+  end
 end
 
 config :podbump, Podbump.Kubernetes, conn: get_k8s_conn.()
